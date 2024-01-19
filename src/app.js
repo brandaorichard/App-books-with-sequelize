837755814fd0ef8ba6b271b45b2c8099f1faf210
brandaorichard <rih.brandao@gmail.com>
2024-01-19 04:43:08 -0300

    feat: relationships N:N

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const userBookController = require('./controllers/userBook.controller');
+
 const app = express();
 app.use(express.json());
 
 app.get('/health/live', (req, res) => res.sendStatus(200));
+
+app.get('/userbooks/:id', userBookController.getUsersBooksById);
 
 module.exports = app;
